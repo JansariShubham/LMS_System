4123f8ebeb215add0323a54c771ac8f1ae0c6206
Keval Shah <kevalshah2161@gmail.com>
2022-06-24 20:40:21 +0530

    phone number length removed

--- LMS/src/main/java/com/spring/lms/model/User.java
+++ LMS/src/main/java/com/spring/lms/model/User.java
@@ -29,7 +29,7 @@ public class User {
 	@Column(nullable = false)
 	private String role;
 
-	@Column(nullable = true, length = 10)
+	@Column(nullable = true)
 	private String phoneNum;
 
 	@Transient
