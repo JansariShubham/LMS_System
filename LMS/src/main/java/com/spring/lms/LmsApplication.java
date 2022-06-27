e0bd69558274d90a26d7af79366758ac5c882bbf
sunny-aims <sunny.itaims@gmail.com>
2022-06-27 18:59:56 +0530

    Reading from property file

--- LMS/src/main/java/com/spring/lms/LmsApplication.java
+++ LMS/src/main/java/com/spring/lms/LmsApplication.java
@@ -3,6 +3,8 @@ package com.spring.lms;
 //import java.time.LocalDate;
 //import java.util.Date;
 
+import com.spring.lms.utility.EmailUtility;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -12,9 +14,18 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class LmsApplication {
 
+	private static EmailUtility utility;
+
+	@Autowired
+	public void setUtility(EmailUtility util){
+		utility = util;
+		System.out.println("\nSetter injection");
+	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(LmsApplication.class, args);
 		System.out.println("Application started...");
+		utility.sendEmail("sunnyvachheta89@gmail.com", "This is test email", "Hello there....");
 	}
 
 }
