dc8bf78e25d96c0c139f40cebf39d26e6ef70dbe
sunny-aims <sunny.itaims@gmail.com>
2022-07-06 12:06:27 +0530

    Native Query update

--- LMS/src/main/java/com/spring/lms/repository/UserReviewsRepo.java
+++ LMS/src/main/java/com/spring/lms/repository/UserReviewsRepo.java
@@ -10,7 +10,7 @@ import java.util.List;
 @Repository
 public interface UserReviewsRepo extends JpaRepository<UserReviews, Integer> {
 
-    @Query(value = "SELECT SUM(u.courseRating), COUNT(*) FROM UserReviews AS u WHERE u.courseId = ?1")
+    @Query(value = "SELECT SUM(u.courseRating), COUNT(*) FROM UserReviews AS u WHERE u.courseId = ?1", nativeQuery = true)
     Integer[] countTotalByCourseId(Integer courseId);
 
     List<UserReviews> findByCourseId(Integer courseId);
