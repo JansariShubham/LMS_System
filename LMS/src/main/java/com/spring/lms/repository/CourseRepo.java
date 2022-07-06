7fc95e91de1a02b8bc6445f79aec1bd67ce69b83
Keval Shah <kevalshah2161@gmail.com>
2022-07-06 20:50:10 +0530

    format doc

--- LMS/src/main/java/com/spring/lms/repository/CourseRepo.java
+++ LMS/src/main/java/com/spring/lms/repository/CourseRepo.java
@@ -16,9 +16,7 @@ import com.spring.lms.model.Course;
 public interface CourseRepo extends JpaRepository<Course, Integer>{
 
 	Course findBycourseName(String courseName);
-
-	//@Query("SELECT new com.spring.lms.dto.CoursesDTO(c.courseId, c.courseName, c.courseDuration, c.courseRating, c.courseStatus, c.coursePrice, c.courseImage, u.firstName, u.profileImage)  FROM Course c INNER JOIN Tutor t ON (c.tutor_fk = t.tutor_id) INNER JOIN User u ON (t.user_fk = u.user_id) ")
-//	@Query(value = "SELECT c.course_id, c.course_name, c.course_duration,c.course_date,c.course_description, c.course_rating, c.course_status, c.course_price, c.course_image, u.first_name,u.last_name, u.profile_image FROM Course as c INNER JOIN Tutor as t ON (c.tutor_fk = t.tutor_id) INNER JOIN User as u ON (t.user_fk = u.user_id)",nativeQuery = true)
+	
 	@Query("SELECT new com.spring.lms.dto.CoursesDTO(c.courseId, c.courseName, c.courseDescription, c.coursePrice, c.courseDuration, c.courseDate, c.courseImage, c.courseRating, c.courseStatus, u.firstName, u.lastName, u.profileImage) "
 			+ "FROM Course as c "
 			+ "INNER JOIN Tutor as t ON (c.tutor = t.tutor_id) "
@@ -30,34 +28,5 @@ public interface CourseRepo extends JpaRepository<Course, Integer>{
 			+ " INNER JOIN Tutor as t ON (c.tutor = t.tutor_id)"
 			+ " INNER JOIN User as u ON (t.user = u.user_id)"
 			+ " WHERE c.courseId = :courseId")
-	CoursesDTO getCourse(int courseId);
-	
+	CoursesDTO getCourse(int courseId);	
 }
-//	
-//	@NamedNativeQuery(name = "getCourses", query = "SELECT new com.spring.lms.dto.CoursesDTO(c.course_id, c.course_name, c.course_duration,c.course_date,c.course_description, c.course_rating, c.course_status, c.course_price, c.course_image, u.first_name,u.last_name, u.profile_image)  FROM Course as c INNER JOIN Tutor as t ON (c.tutor_fk = t.tutor_id) INNER JOIN User as u ON (t.user_fk = u.user_id)")
-//	List<CoursesDTO> getCourses();
-//}
-
-//@NamedNativeQuery(
-//	    name = "find_stock_akhir_dto",
-//	    query =
-//	        "SELECT " + 
-//	        "  stock_akhir.product_id AS productId, " + 
-//	        "  stock_akhir.product_code AS productCode, " + 
-//	        "  SUM(stock_akhir.qty) as stockAkhir " + 
-//	        "FROM book_stock stock_akhir " + 
-//	        "where warehouse_code = :warehouseCode " + 
-//	        "  AND product_code IN :productCodes " + 
-//	        "GROUP BY product_id, product_code, warehouse_id, warehouse_code",
-//	    resultSetMapping = "stock_akhir_dto"
-//	)
-//	@SqlResultSetMapping(
-//	    name = "stock_akhir_dto",
-//	    classes = @ConstructorResult(
-//	        targetClass = StockAkhirDto.class,
-//	        columns = {
-//	            @ColumnResult(name = "productId", type = Long.class),
-//	            @ColumnResult(name = "productCode", type = String.class),
-//	            @ColumnResult(name = "stockAkhir", type = Integer.class)
-//	        }
-//	    )
