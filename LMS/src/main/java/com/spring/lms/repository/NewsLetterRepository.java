8074992279f311560647e6e908341a2d2c8be8bb
sunny-aims <sunny.itaims@gmail.com>
2022-07-18 18:54:26 +0530

    Removed bug in NewsLetter and UserReview

--- LMS/src/main/java/com/spring/lms/repository/NewsLetterRepository.java
+++ LMS/src/main/java/com/spring/lms/repository/NewsLetterRepository.java
@@ -2,9 +2,11 @@ package com.spring.lms.repository;
 
 import com.spring.lms.model.NewsLetter;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import javax.transaction.Transactional;
 import java.util.List;
 import java.util.Optional;
 
@@ -12,6 +14,8 @@ import java.util.Optional;
 public interface NewsLetterRepository extends JpaRepository<NewsLetter, Long> {
     Optional<NewsLetter> findByEmail(String email);
 
+    @Modifying
+    @Transactional
     void deleteByEmail(String email);
 
     @Query(
--- LMS/src/main/java/com/spring/lms/repository/UserReviewsRepo.java
+++ LMS/src/main/java/com/spring/lms/repository/UserReviewsRepo.java
@@ -2,9 +2,11 @@ package com.spring.lms.repository;
 
 import com.spring.lms.model.UserReviews;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import javax.transaction.Transactional;
 import java.util.List;
 
 @Repository
@@ -15,5 +17,7 @@ public interface UserReviewsRepo extends JpaRepository<UserReviews, Integer> {
 
     List<UserReviews> findByCourseId(Integer courseId);
 
+    @Modifying
+    @Transactional
     void deleteByReviewId(Integer reviewId);
 }
