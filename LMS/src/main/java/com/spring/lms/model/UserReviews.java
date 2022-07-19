f19e79866327c6ffea1939bbbba5e87a69b3f6d4
Keval Shah <kevalshah2161@gmail.com>
2022-07-19 17:14:53 +0530

    minor change

--- LMS/src/main/java/com/spring/lms/model/UserReviews.java
+++ LMS/src/main/java/com/spring/lms/model/UserReviews.java
@@ -25,7 +25,6 @@ public class UserReviews implements Serializable
     @Column(name = "courseReview", nullable = true)
     private String reviewDescription;
 
-    @Transient
     private String userName;
 
     public UserReviews() {
